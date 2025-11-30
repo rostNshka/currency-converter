312654a048eb63a09655bd32f5d4b876f6c78ae3
RostNshka <devkotder@ya.ru>
2025-11-30 23:55:25 +0400

    add loading and handling errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import {useState, useEffect} from "react"
 import './App.css'
 
 // fetch(`https://api.frankfurter.dev/v1/latest?base=${from}&symbols=${to}`)
@@ -10,23 +10,50 @@ const App = () => {
   const [fromCurrency, setFromCurrency] = useState("USD")
   const [toCurrency, setToCurrency] = useState("USD")
   const [amount, setAmount] = useState('1')
-  const [convertedAmount, setConvertedAmount] = useState('')
+  const [convertedAmount, setConvertedAmount] = useState('1')
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     async function getCurrencies() {
-      const response = await fetch(`${API_URL}latest`)
-      const data = await response.json()
+      try {
+        const response = await fetch(`${API_URL}latest`)
+        const data = await response.json()
 
-      setCurrencies(Object.keys(data.rates))
+        setCurrencies(Object.keys(data.rates))
+      } catch {
+        setError('Failed to fetch currency data')
+      }
     }
+
     getCurrencies()
-  },[])
+  }, )
 
   async function getConverted() {
-    const response = await fetch(`${API_URL}latest?base=${fromCurrency}&symbols=${toCurrency}`)
-    const data = await response.json()
+    if(!amount || amount <= 0 ){
+      setError("Amount cannot be less than zero.")
+      return
+    }
 
-    setConvertedAmount((data.rates[toCurrency] * amount).toFixed(2))
+    setError(null)
+    setLoading(true)
+
+    if (fromCurrency === toCurrency) {
+      setConvertedAmount(amount)
+      setLoading(false)
+      return
+    }
+
+    try {
+      const response = await fetch(`${API_URL}latest?base=${fromCurrency}&symbols=${toCurrency}`)
+      const data = await response.json()
+
+      setConvertedAmount((data.rates[toCurrency] * amount).toFixed(2))
+    } catch {
+      setError('Failed to convert')
+    } finally {
+      setLoading(false)
+    }
   }
 
   return (
@@ -34,7 +61,7 @@ const App = () => {
       <h1 className="app__title">Currency Converter</h1>
 
       <div className="app__container">
-        <p className="error"></p>
+        {error && <p className="error">{error}</p>}
         <div className="app__container__group">
           <input
             type="number"
@@ -77,9 +104,11 @@ const App = () => {
         </div>
 
         <button className="app__container__button" onClick={getConverted}>Convert</button>
-        <p className="loading">Converting...</p>
+        {loading && <p className="loading">Converting...</p>}
 
-        <p className="result">{convertedAmount}</p>
+        {convertedAmount !== null && !loading &&
+          <p className="result">{amount} {fromCurrency} = {convertedAmount} {toCurrency}</p>
+        }
       </div>
 
     </div>
